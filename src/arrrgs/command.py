d3adb43286c61d5ac4ce9a1ca2e920c21871c4d4
mishamyrt <misha@myrt.co>
2023-08-02 14:58:18 +0300

    Improve root logic
    Preparing to add a subcommand parser

--- src/arrrgs/command.py
+++ src/arrrgs/command.py
@@ -6,7 +6,7 @@ from .arguments import add_args
 from .parser import command_subparsers, parser
 
 
-def register_subcommand(
+def register_handler(
     parent: ArgumentParser,
     func: Callable,
     args: Tuple[str, ...],
@@ -21,13 +21,13 @@ def register_subcommand(
     add_args(cmd_parser, args)
     cmd_parser.set_defaults(func=func)
 
-def command(*args, name:str=None, root=False, parent=command_subparsers):
+def command(*args, name:str=None, root=False, parent=command_subparsers, root_parser=parser):
     """Decorator to define a new command"""
     def decorator(func):
         if root:
-            parser.set_defaults(
+            root_parser.set_defaults(
                 root_command_handler=func,
                 root_command_args=list(args),
             )
-        register_subcommand(parent, func, args, name)
+        register_handler(parent, func, args, name)
     return decorator
